81c913d76d6c854055f4288acd3c1be77ec22c63
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2023-12-30 15:26:16 -0800

    🚨 Fix Linter Errors

--- backend/src/utils/handleError.ts
+++ backend/src/utils/handleError.ts
@@ -1,6 +1,6 @@
 import { Response } from "express";
 
-export const handleError = async (error: any, res: Response) => {
+export const handleError = async (error: unknown, res: Response) => {
     console.error("Error", error);
     return res.status(500).json({ err: "Internal server error" });
 };
